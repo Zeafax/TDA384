5fa34aed7524daf472288d3acd4021a9219c31e0
Jonathan Tunell <carltu@chalmers.se>
2024-10-23 20:17:57 +0200

    Added comments to server_channels and server

--- cchat-skeleton/server.erl
+++ cchat-skeleton/server.erl
@@ -3,38 +3,33 @@
 
 -record(server_st, {
     channels, % list of channels on server
-    nicks
+    nicks % list of nicks on server
 }).
-
-
 
 % Start a new server process with the given name
 % Do not change the signature of this function.
 start(ServerAtom) ->
-    % TODO Implement function
     % - Spawn a new process which waits for a message, handles it, then loops infinitely
     % - Register this process to ServerAtom
     % - Return the process ID
     InitialState = #server_st{
-            channels = [],
-            nicks = []
+            channels = [], % list of channels on server
+            nicks = [] % list of nicks on server
         },
     genserver:start(ServerAtom,InitialState,fun handler/2).
 
 handler(State, {join, Channel, Client, Nick}) ->
-    %receive Msg -> io:format("Received: ~p~n", [Msg]) end.
     % Return if join is sucessful or not
     % create a channel process
-    Result = lists:member(Channel, State#server_st.channels), 
-    case Result of
+    case lists:member(Channel, State#server_st.channels) of % check if channel exists
         true ->
             % channel exists exists
             % check if user alredy there 
             case genserver:request(list_to_atom(Channel), {join, Client}) of
                 joined ->
-                    {reply, joined,  State#server_st{nicks = [Nick|State#server_st.nicks]}};
+                    {reply, joined,  State#server_st{nicks = [Nick|State#server_st.nicks]}}; % user joined
                 failed ->
-                    io:format("~ts~n", ["Failed" ++ Channel]),
+                    io:format("~ts~n", ["Failed" ++ Channel]), % user already in channel
                     {reply, failed, State}
             end;
         false ->
@@ -43,21 +38,20 @@ handler(State, {join, Channel, Client, Nick}) ->
             {reply, joined, State#server_st{channels = [Channel | State#server_st.channels],nicks = [Nick|State#server_st.nicks]}}
     end;
 
-handler(State, {nick, Nick, NewNick}) ->
-    Result = lists:member(NewNick,State#server_st.nicks),
-    case Result of
+handler(State, {nick, Nick, NewNick}) -> 
+    case lists:member(NewNick,State#server_st.nicks) of  % check if new nick is already taken
         true ->
-            {reply, failed, State};
+            {reply, failed, State}; % new nick is already taken
         false ->
-            {reply, ok, State#server_st{nicks = lists:delete(Nick,[NewNick | State#server_st.nicks])}}
+            {reply, ok, State#server_st{nicks = lists:delete(Nick,[NewNick | State#server_st.nicks])}} % change nick
     end;
 
 handler(State, terminate_channels) ->
-    lists:foreach(fun(Channel) ->genserver:stop(list_to_atom(Channel)) end, State#server_st.channels),
+    lists:foreach(fun(Channel) ->genserver:stop(list_to_atom(Channel)) end, State#server_st.channels), % stop all channels
     {reply, ok, State}.
 
 % Stop the server process registered to the given name,
 % together with any other associated processes
 stop(ServerAtom) ->
-    genserver:request(ServerAtom, terminate_channels),
-    genserver:stop(ServerAtom).
+    genserver:request(ServerAtom, terminate_channels),  % stop all channels
+    genserver:stop(ServerAtom).  % stop the server
--- cchat-skeleton/server_channel.erl
+++ cchat-skeleton/server_channel.erl
@@ -8,35 +8,33 @@
 
 % Start a new server channel process with the given name
 start(ServerAtom,Client) ->
-    
-    genserver:start(ServerAtom,#ch_server_st{users = [Client]},fun handler/2).
+    genserver:start(ServerAtom,#ch_server_st{users = [Client]},fun handler/2). % Start the server channel process with the given name
 
-    
+% Function to leave the channel    
 handler(State, {leave, Client}) ->
-    Result = lists:member(Client, State#ch_server_st.users),
-    case Result of
+    case lists:member(Client, State#ch_server_st.users) of % Check if the user is in the channel 
         true ->
-            {reply, ok, State#ch_server_st{users = lists:delete(Client, State#ch_server_st.users)}};
-        false ->
-            {reply, failed, State}
+            {reply, ok, State#ch_server_st{users = lists:delete(Client, State#ch_server_st.users)}};  % Remove the user from the channel
+        false -> 
+            {reply, failed, State} % User is not in the channel
     end;
 
+% Function to join the channel
 handler(State, {join, Client})->
-    Result = lists:member(Client, State#ch_server_st.users),
-    case Result of
+    case lists:member(Client, State#ch_server_st.users) of % Check if the user is already in the channel
         true ->
-            {reply, failed,  State};
+            {reply, failed,  State}; % User is already in the channel
         false ->
-            {reply, joined,  State#ch_server_st{users = [Client | State#ch_server_st.users]}}
+            {reply, joined,  State#ch_server_st{users = [Client | State#ch_server_st.users]}} % Add the user to the channel
     end;
 
-
+% Function to messages in the channel
 handler(State, {handle_message,Client, Nick, Channel, Msg}) ->
-    case lists:member(Client, State#ch_server_st.users) of
+    case lists:member(Client, State#ch_server_st.users) of % Check if the user is in the channel
         true->
              lists:foreach(fun(ChClient) -> % Send the message to all users in the channel
                         spawn(fun() -> genserver:request(ChClient, {message_receive, Channel, Nick, Msg}) end) % Send the message, spawn a new process for cuncurrency
-                    end, lists:delete(Client, State#ch_server_st.users)),
+                    end, lists:delete(Client, State#ch_server_st.users)), % Send the message to all users except the sender
                 {reply, ok, State};
         false ->
             % Channel doesn't exist
